94a0b08149c30ac82aacee84aee6a93268a6ee37
perry <samesense@gmail.com>
2021-04-15 10:33:08 -0400

    :tada: Initial commit

---
+++ conda_activator_py/__main__.py
@@ -0,0 +1,66 @@
+# type: ignore[attr-defined]
+
+from typing import Optional
+
+import random
+from enum import Enum
+
+import typer
+from conda_activator_py import __version__
+from conda_activator_py.example import hello
+from rich.console import Console
+
+
+class Color(str, Enum):
+    white = "white"
+    red = "red"
+    cyan = "cyan"
+    magenta = "magenta"
+    yellow = "yellow"
+    green = "green"
+
+
+app = typer.Typer(
+    name="conda-activator-py",
+    help="Awesome `conda-activator-py` is a Python cli/package created with https://github.com/TezRomacH/python-package-template",
+    add_completion=False,
+)
+console = Console()
+
+
+def version_callback(value: bool):
+    """Prints the version of the package."""
+    if value:
+        console.print(
+            f"[yellow]conda-activator-py[/] version: [bold blue]{__version__}[/]"
+        )
+        raise typer.Exit()
+
+
+@app.command(name="")
+def main(
+    name: str = typer.Option(..., help="Name of person to greet."),
+    color: Optional[Color] = typer.Option(
+        None,
+        "-c",
+        "--color",
+        "--colour",
+        case_sensitive=False,
+        help="Color for name. If not specified then choice will be random.",
+    ),
+    version: bool = typer.Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Prints the version of the conda-activator-py package.",
+    ),
+):
+    """Prints a greeting for a giving name."""
+    if color is None:
+        # If no color specified use random value from `Color` class
+        color = random.choice(list(Color.__members__.values()))
+
+    greeting: str = hello(name)
+    console.print(f"[bold {color}]{greeting}[/]")
